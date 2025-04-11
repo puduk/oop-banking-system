10ef0c76c70066a009df0cfbc6c0dfa921145447
Mert Demir <demirmert986@gmail.com>
2025-04-11 03:01:19 +0200

    Add user.cpp

---
+++ Utils/user.cpp
@@ -0,0 +1,16 @@
+#include "user.hpp"
+
+user::user(){
+  name = "";
+  surname = "";
+  age = 0;
+  user_id = 0;
+  card_number = 0;
+  card_ccv = 0;
+  card_date = "";
+  current_balance = 0;
+}
+
+
+
+
